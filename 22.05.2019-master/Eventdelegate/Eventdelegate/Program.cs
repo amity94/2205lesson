f120cabac4a68551b540c6b699c02e7c148ade74
amity94 <45635119+amity94@users.noreply.github.com>
2019-05-22 20:11:54 +0300

    Add files via upload

---
+++ 22.05.2019-master/Eventdelegate/Eventdelegate/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventdelegate
+{
+    class Program
+    {
+        public static void AfterVideoEncoded(Video video)
+        {
+            Console.WriteLine($"my delegate -- {video} was encoded! yeah!");
+        }
+
+        // 3 void [what-happend]-EventHandler (object sender, EventArgs e)
+        public static void VideoEncodedEventHandler(object sender, VideoEventArgs e)
+        {
+            Console.WriteLine(sender);
+            Console.WriteLine($"MAIN -------- Video was encoded!!!! {e.Video}");
+        }
+        public static void EmailSentEventHandler(object sender, VideoEventArgs e)
+        {
+            Console.WriteLine(sender);
+            Console.WriteLine($"Email was sent {e.Video}");
+        }
+        public static void VideoEncodingEventHandler(object sender, VideoEventArgs e)
+        {
+            Console.WriteLine(sender);
+            Console.WriteLine($"MAIN--------- Video is Encoding..... encoding");
+        }
+        static void Main(string[] args)
+        {
+
+            VideoEncoder encode = new VideoEncoder();
+            Video video = new Video("My cool video");
+
+            // 4 registrating the method to the Publisher
+            encode.VideoEncoded += VideoEncodedEventHandler;
+            encode.VideoEncoded += EmailSentEventHandler;
+            encode.VideoEncoding += VideoEncodingEventHandler;
+
+            encode.Encode(video);
+
+        }
+    }
+}
---
+++ 22.05.2019-master/Eventdelegate/Eventdelegate/VideoEncoder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventdelegate
+{
+    class VideoEncoder
+    {
+        // 1  - instance of the delegate (event)
+        // subscribe to this!
+        // event EventHandler [what-happend?]
+        // delegate void EventHandler(object sender, VideoEventArgs e);
+        public event EventHandler<VideoEventArgs> VideoEncoded;
+        public event EventHandler<VideoEventArgs> VideoEncoding;
+
+        // 2 - On[what-happend?]
+        public void OnVideoEncoded(Video video)
+        {
+            //VideoEventArgs { Video = video } 
+            // shortcut for:
+            //VideoEventArgs v = new VideoEventArgs();
+            //v.Video = video;
+
+            if (VideoEncoded != null)
+            {
+                VideoEncoded.Invoke(this, new VideoEventArgs { Video = video });
+            }
+        }
+
+        public void OnVideoEncoding(Video video)
+        {
+            if (VideoEncoding != null)
+            {
+                VideoEncoding.Invoke(this, new VideoEventArgs { Video = video });
+            }
+        }
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Starting to Encode video ...");
+            OnVideoEncoding(video);
+
+            // starting to encode
+            Console.WriteLine("Encoding video ...");
+
+            OnVideoEncoded(video);
+        }
+    }
+}
